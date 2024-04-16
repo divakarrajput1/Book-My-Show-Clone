ee22a29b06cc99f981ebfa4f254bfff072eabc53
divakarrajput1 <152204366+divakarrajput1@users.noreply.github.com>
2024-04-16 07:07:45 +0000

    Updated Code

---
+++ book-my-show-clone/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+//react-slick CSS
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return <div>
+    <h1>Hello World</h1>
+   </div>
+}
+
+export default App;
