82d564e80cc723912ded60fc1b8f1968a994bdf3
divakarrajput1 <152204366+divakarrajput1@users.noreply.github.com>
2024-04-18 21:58:41 +0000

    Final code

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Book-My-Show-Clone
+
+# cmds
+npx create-react-app (project name)
+cd (project name)
+npm start
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init
+
+# react-slick
+npm install react-slick
+npm install slick-carousel
+
+# axios 
+npm i axios
+
+# headless ui
+npm install @headlessui/react
+
+# react-icons
+npm install react-icons
+
+# react router dom 
+npm i react-router-dom
+
+# Resources 
+https://tailwindcss.com/docs/installation
+https://react-slick.neostack.com/docs/get-started
+https://axios-http.com/docs/intro
+https://headlessui.com/
--- book-my-show-clone/src/pages/Movie.page.jsx
+++ book-my-show-clone/src/pages/Movie.page.jsx
@@ -1,12 +1,127 @@
-import React from 'react'
+import React, { useEffect, useState, useContext}  from 'react'
+
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { MovieContext } from "../context/Movie.Context";
+import Slider from "react-slick";
+import {FaCcVisa, FaCcApplePay} from "react-icons/fa"
+import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
 
 //hoc
 import MovieLayoutHoc from '../layout/Movie.layout'
 
-const Moviepage = () => {
-  return (
-    <div>Moviepage</div>
-  )
-}
+const MoviePage = () => {
+    const { id } = useParams();
 
+    const {movie, setMovie} = useContext(MovieContext);
+
+    const [cast, setCast] = useState([]);
+    const [similarMovies, setSimilarMovies] = useState([]);
+    const [recommendedMovies, setRecommendedMovies] = useState([]);
+
+     useEffect(()=> {
+        const requestCast = async () => {
+            const getCast = await axios.get(`/movie/${id}/credits`);
+            // const getCast = await axios.get(`/movie/500/credits`);
+            setCast(getCast.data.cast);
+        };
+        requestCast();
+    }, [id]);
+
+    useEffect(()=> {
+        const requestSimilarMovies = async () => {
+            const getSimilarMovies = await axios.get(`/movie/${id}/similar`);
+            setSimilarMovies(getSimilarMovies.data.results);
+        };
+        requestSimilarMovies();
+    }, [id]);
+
+
+     useEffect(()=> {
+        const requestRecommendedMovies = async () => {
+            const getRecommendedMovies = await axios.get(`/movie/${id}/recommendations`);
+            setRecommendedMovies(getRecommendedMovies.data.results);
+        };
+        requestRecommendedMovies();
+    }, [id]);
+
+    return  (
+             <>
+    <MovieHero />
+            <div className="my-12 container px-4 lg:ml-20 lg:w-2/1">
+                <div className="flex flex-col items-start gap-3">
+                    <h1 className="text-gray-800 font-bold text-2xl">About the movie</h1>
+                    <p>{movie.overview}</p>
+                </div>
+
+                <div className="my-8">
+                    <hr />
+                </div>
+
+                <div className="my-8">
+                <h2 className="text-gray-800 font-bold text-2xl mb-3">Applicable Offers</h2>
+                <div className="flex flex-col gap-3 lg:flex-row">
+                    <div className="flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+                        <div className="w-8 h-8">
+                            <FaCcVisa className="w-full h-full"/>
+                            </div>
+                            <div className="flex flex-col items-start">
+                                <h3 className="text-gray-700 text-xl font-bold">Visa Stream Offer</h3>
+                                <p className="text-gray-600">Get 50% off up to INR 150 on all RuPay card* on BookMyShow Stream.</p>
+                            </div>
+                    </div>
+
+                    <div className="flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md">
+                        <div className="w-8 h-8">
+                            <FaCcApplePay className="w-full h-full"/>
+                            </div>
+                            <div className="flex flex-col items-start">
+                                <h3 className="text-gray-700 text-xl font-bold">Film Pass</h3>
+                                <p className="text-gray-600">Get 50% off up to INR 150 on all RuPay card* on BookMyShow Stream.</p>
+                            </div>
+                    </div>
+                </div>
+                </div>
+
+                <div className="my-8">
+                    <hr />
+                </div>
+
+                {/* Cast & Crew */}
+                <div className="my-8">
+                    <h2 className="text-gray-800 font-bold text-2xl mb-4">
+                        Cast & Crew
+                    </h2>
+                    <Slider {...settingsCast}>
+                        {cast.map((castData)=>{
+                            <Cast image={castData.profile_path} castName={castData.original_name} role={castData.character} />
+                        })}
+                    </Slider>
+                </div>
+
+                 <div className="my-8">
+                    <hr />
+                </div>
+
+                {/* Movies */}
+                <div className="my-8">
+                    <PosterSlider config={settings} title="Recommended Movies" posters={recommendedMovies} isDark ={false}/>
+                </div>
+
+                 <div className="my-8">
+                    <hr />
+                </div>
+
+                {/* Movies */}
+                 <div className="my-8">
+                    <PosterSlider config={settings} title="BMS XCLUSICE" posters={similarMovies} isDark ={false}/>
+                </div>
+
+                 <div className="my-8">
+                    <hr />
+                </div>
+            </div>
+        </>
+    )
+  };
 export default MovieLayoutHoc(Moviepage);
